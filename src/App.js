50f65d59ccbab3eda2856cc1c8bb16d55b9271b3
Foysal Ahamed Badsha <you@example.com>
2022-05-24 04:58:27 +0600

    Login complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login';
+import MyProtfolio from './Pages/MyProtfolio';
+import RequireAuth from './Pages/RequireAuth';
 import Navbar from './Pages/Shared/Navbar';
+import NotFound from './Pages/Shared/NotFound';
+import SignUp from './Pages/SignUp';
 function App() {
   return (
     <div className='px-8'>
@@ -10,6 +14,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/myprotfolio' element={
+          <RequireAuth>
+            <MyProtfolio></MyProtfolio>
+          </RequireAuth>
+        }></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,10 +1,20 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const logout = () => {
+        signOut(auth);
+    }
+
     const manufacters = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        <li>{user ? <button class="btn btn-active btn-ghost" onClick={logout}>Sign Out</button> : <Link to='/login'>Login</Link>}</li>
+        <li><Link to='/myprotfolio'>My-protfolio</Link></li>
     </>
     return (
         <div className="navbar bg-accent">
